c400d2ad1ead926445fe26adfd4d7aac76dc49ec
Abe <shalashtein@gmail.com>
2020-12-02 12:12:03 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+puts "scientific" + "   " + "english" + "   " + "arabic" + "   " + "texture" + "   " + "flavor"
+CSV.foreach('fish.csv') do |row|
+  puts row [2]
+  scientific = row[0]
+  scientific ||= ""
+  english = row[1]
+  english ||= ""
+  arabic = row[2]
+  arabic ||= ""
+  texture = row[3]
+  texture ||= ""
+  flavor = row[4]
+  flavor ||= ""
+  Fish.create(scientific: scientific, english: english, lebanese: arabic, texture: texture, flavor: flavor).save!
+end
+puts "done"
