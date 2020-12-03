f09d0a7aa4dec9191306343db68bf0102352ac4e
Abe <shalashtein@gmail.com>
2020-12-03 15:08:04 +0200

    navbar edits, added modal to cart button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :market ]
+  #before_action :current_cart, only: [ :market ]
+
+  def current_cart
+    if session[:cart_id]
+      @current_cart ||= Cart.find(session[:cart_id])
+    end
+    if session[:cart_id].nil?
+      @current_cart = Cart.create!
+      session[:cart_id] = @current_cart.id
+    end
+    @current_cart
+  end
 
   def market
+    @products = Product.all
   end
 end
--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,4 +1,4 @@
-class CatchPolicy < ApplicationPolicy
+class ProductPolicy < ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
